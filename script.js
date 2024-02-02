d2a5a01c91f5d6d2458459bbd1c21a124d527fbc
Daiana de Paula <daianaadepaula1@gmail.com>
2024-02-02 13:08:04 -0300

    feat: size box and title

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const imagemVisualizacao = document.getElementById('imagem-visualizacao');
+const tituloProduto = document.getElementById('titulo-produto');
 
 const verdeCipreste = {
 	nome: 'Verde-cipreste',
@@ -26,11 +27,28 @@ const rosaClaro = {
 };
 
 const opcoesCores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro];
+const opcoesTamanho = ['41 mm', '45 mm'];
 
 let imagemSelecionada = 1;
+let tamanhoSelecionado = 1;
 
 function trocarImagem(){
 	const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
 	imagemSelecionada = idOpcaoSelecionada.charAt(0);
 	imagemVisualizacao.src = `./imagens/opcoes-cores/imagens-azul-inverno/imagem-${imagemSelecionada}.jpeg`
+}
+
+function trocarTamanho(){
+	//atualizar variável de controle do tamanho da caixa
+	const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
+	tamanhoSelecionado = idOpcaoSelecionada.charAt(0);
+	//mudar o título do produto
+	tituloProduto.innerText = 'Pulseira loop esportiva azul-inverno para caixa de ' + opcoesTamanho[tamanhoSelecionado]
+	//mudar o tamanho da jaqueta de acordo com a opção
+	if(opcoesTamanho[tamanhoSelecionado] === '41 mm'){
+		imagemVisualizacao.classList.add('caixa-pequena');
+	} else {
+		imagemVisualizacao.classList.remove('caixa-pequena');		
+	}
+	
 }
